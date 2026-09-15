99eab2127f7994f67a48c2762f1f73fb41fe9e1b
Mayank Joshi <125684136+13mayankjoshi13@users.noreply.github.com>
2026-09-15 22:00:20 +0530

    Add Linear Regression model example in Lecture_1.py

---
+++ ML/Lecture_1.py
@@ -0,0 +1,19 @@
+from sklearn.linear_model import LinearRegression
+
+# Training data
+X = [[1], [2], [3], [4], [5]]
+Y = [20, 30, 40, 50, 60]
+
+# Create model
+model = LinearRegression()
+
+# Train the model
+model.fit(X, Y)
+
+# New X value
+new_x = [[6]]
+
+# Predict Y
+prediction = model.predict(new_x)
+
+print("Predicted Y:", prediction[0])
