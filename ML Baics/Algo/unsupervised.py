275a8d6b22e978a8f16c162d6e2ea24878a9b0d6
Mayank Joshi <125684136+13mayankjoshi13@users.noreply.github.com>
2026-09-22 21:48:12 +0530

    Implement KMeans clustering in unsupervised.py

--- ML Baics/Algo/unsupervised.py
+++ ML Baics/Algo/unsupervised.py
@@ -1,1 +1,16 @@
+from sklearn.cluster import KMeans
 
+# [Study Hours, Attendance]
+X = [
+    [2, 60],
+    [3, 65],
+    [4, 70],
+    [8, 90],
+    [9, 95],
+    [10, 92]
+]
+
+model = KMeans(n_clusters=2, random_state=0)
+model.fit(X)
+
+print(model.labels_)
